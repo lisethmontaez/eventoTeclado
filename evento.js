7d82f7f69662aedf61e8ad0ceaea768146b574ea
LsPartida <beluis1993@gmail.com>
2019-07-19 16:59:54 -0500

    Se logró realizar el dibujado en diagonal y se controla el pulso de direcciones contrarias

--- evento.js
+++ evento.js
@@ -1,13 +1,32 @@
-document.addEventListener("keydown", dibujarTeclado);
+document.addEventListener("keydown", pulsar);
+document.addEventListener("keyup", soltar);
 
+var pulsos=new Array(4).fill(false);
+// 0 up
+// 1 down
+// 2 left
+// 3 right
 var teclas = { UP: 38, DOWN: 40, RIGHT: 39, LEFT: 37 };
 var cuadrito = document.getElementById("area_trabajo");
 var papel = cuadrito.getContext("2d");
 var x = 100;
 var y = 100;
+var velx=0;
+var vely=0;
+var colorcito = "#BE3BF9";
+var movimiento = 5;
 
-dibujarLinea("red", x-1, y-1 , x+1, y+1, papel);
-
+dibujarLinea("red", x-1, y-1 , x+1, y+1, papel);//Iniciar trazo en el centro
+function pulsado()
+{
+	for(var i=0;i<pulsos.length;i++)
+	{
+		if(pulsos[i])
+			return true;
+			console.log("pulsado");
+	}
+	return false;
+}
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo){
 	lienzo.beginPath();
 	lienzo.strokeStyle = color;
@@ -17,44 +36,86 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo){
 	lienzo.stroke();
 	lienzo.closePath();
 } 
-
-ffunction dibujarTeclado(evento){           
-	var colorcito = "#BE3BF9";
-	var movimiento = 5;
-	var arribaderecha = UP + RIGHT;
-	var arribaizquiera = UP + LEFT;
-	var abajoderecha = DOWN + RIGHT;
-	var abajoizquierda = DOWN + LEFT;
-
-	switch(evento.keyCode){
-
-		case teclas.DOWN:
-		dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
-		y = y + movimiento;
-		break;
-		case teclas.UP:
-		dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
-		y = y - movimiento;
-		break;
-		case teclas.LEFT:
-		dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
-		x = x - movimiento;
-		break;
-		case teclas.RIGHT:
-		dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
-		x = x + movimiento;
-		case teclas.arribaderecha:
-		dibujarLinea(colorcito, x, y + movimiento, x + movimiento, y, papel);
-		break;
-		case teclas.arribaizquiera:
-		dibujarLinea(colorcito, x, y + movimiento, x - movimiento, y, papel);
-		break;
-		case teclas.abajoderecha:
-		dibujarLinea(colorcito, x, y - movimiento, x + movimiento, y, papel);
-		break;
-		case teclas.abajoizquierda:
-		dibujarLinea(colorcito, x, y - movimiento, x - movimiento, y, papel);
-		break;
-
+function pulsar(evento)
+{
+	if(evento.keyCode==teclas.UP)
+	{
+		vely=-5;
+		// yf-=movimiento;
+		pulsos[0]=true;
 	}
+	if(evento.keyCode==teclas.DOWN)
+	{
+		// dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+		// yf += movimiento;
+		vely=5;
+		pulsos[0]=true;
+	}
+	if(evento.keyCode==teclas.LEFT)
+	{
+		// dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+		// xf -= movimiento;
+		velx=-5;
+		pulsos[0]=true;
+	}
+	if(evento.keyCode==teclas.RIGHT)
+	{
+		// dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+		// xf += movimiento;
+		velx=5;
+		pulsos[0]=true;
+	}
+	// if(pulsos[0] && pulsos[1])
+	// {
+	// 	vely=0;
+	// }
+	// if(pulsos[2] && pulsos[3])
+	// {
+	// 	velx=0;
+	// }
+	console.log("velx:" +velx);
+	console.log("vely:" +vely);
+	dibujarLinea(colorcito,x,y,x+velx,y+vely,papel);
+	x+=velx;
+	y+=vely;
 }
+function soltar(evento)
+{
+
+	console.log("Soltado: ");
+	if(evento.keyCode==teclas.UP)
+	{
+		vely+=5;
+		// yf-=movimiento;
+		pulsos[0]=false;
+		console.log("UP");
+	}
+	if(evento.keyCode==teclas.DOWN)
+	{
+		// dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+		// yf += movimiento;
+		vely-=5;
+		pulsos[0]=false;
+		console.log("DOWN");
+	}
+	if(evento.keyCode==teclas.LEFT)
+	{
+		// dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+		// xf -= movimiento;
+		velx+=5;
+		pulsos[0]=false;
+		console.log("LEFT");
+	}
+	if(evento.keyCode==teclas.RIGHT)
+	{
+		// dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+		// xf += movimiento;
+		velx-=5;
+		pulsos[0]=false;
+		console.log("RIGHT");
+	}
+	
+	console.log("velx:" +velx);
+	console.log("vely:" +vely);
+}
+	
